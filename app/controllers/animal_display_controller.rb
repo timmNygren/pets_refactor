d01ba41abc878ff0cf7a7588009c7d2f83dd110c
Hunter Lang <hunter@orbotix.com>
2014-03-24 19:44:49 -0600

    Animal display controller commented.

--- app/controllers/animal_display_controller.rb
+++ app/controllers/animal_display_controller.rb
@@ -1,9 +1,17 @@
+# Controller that shows the animal display
+
 class AnimalDisplayController < ApplicationController
+
+  # Get the current considering, so it can be displayed
   include CurrentConsidering
+
+  # Ensure that the considering is set before continuing with display
   before_action :set_considering
 
-    helper_method :sort_column, :sort_direction
+  # Helper method declaration so that pets can be sorted
+  helper_method :sort_column, :sort_direction
   
+  # Index action, automatically sorts the page 
   def index
     @pets = Pet.order(sort_column + " " + sort_direction)
   end
@@ -12,10 +20,12 @@ class AnimalDisplayController < ApplicationController
   
   private
   
+  # Helper method to sort by column
   def sort_column
     Pet.column_names.include?(params[:sort]) ? params[:sort] : "name"
   end
   
+  # Helper method to choose ascending or descending
   def sort_direction
     %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
